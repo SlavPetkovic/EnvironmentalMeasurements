d00a78a1dc9a1345a71bdbc95a34c90b4400f570
SlavPetkovic <slavoljub.petkovic@outlook.com>
2022-03-19 02:31:35 -0400

    Test and Main Update

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+import pandas as pd
+from datetime import datetime, timedelta
+import time
+from lib.etl import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+def etl(name):
+    data = sensorsreading()
+    dataload(data)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    starttime = time.time()
+    while True:
+        etl('PyCharm')
+        dt = datetime.now() + timedelta(minutes=1)
+        dt = dt.replace(second=1)
+
+        while datetime.now() < dt:
+            time.sleep(1)
+    # Re-run the script at the beginning of every new minute.
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
